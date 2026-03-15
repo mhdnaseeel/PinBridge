6c1294d53daf28b4bdfffb376e28d6447c9ad495
mhdnaseeel <mhdnaseel521@gmail.com>
2026-03-15 15:25:22 +0530

    Update generated build artifacts, including Hilt code, merged localized resources, and app metadata.

--- android/app/build/generated/ksp/debug/java/com/pinbridge/otpmirror/MainActivity_MembersInjector.java
+++ android/app/build/generated/ksp/debug/java/com/pinbridge/otpmirror/MainActivity_MembersInjector.java
@@ -1,7 +1,6 @@
 package com.pinbridge.otpmirror;
 
-import android.content.SharedPreferences;
-import com.google.firebase.auth.FirebaseAuth;
+import com.pinbridge.otpmirror.data.PairingRepository;
 import dagger.MembersInjector;
 import dagger.internal.DaggerGenerated;
 import dagger.internal.InjectedFieldSignature;
@@ -24,34 +23,25 @@ import javax.inject.Provider;
     "deprecation"
 })
 public final class MainActivity_MembersInjector implements MembersInjector<MainActivity> {
-  private final Provider<FirebaseAuth> authProvider;
+  private final Provider<PairingRepository> pairingRepositoryProvider;
 
-  private final Provider<SharedPreferences> prefsProvider;
-
-  public MainActivity_MembersInjector(Provider<FirebaseAuth> authProvider,
-      Provider<SharedPreferences> prefsProvider) {
-    this.authProvider = authProvider;
-    this.prefsProvider = prefsProvider;
+  public MainActivity_MembersInjector(Provider<PairingRepository> pairingRepositoryProvider) {
+    this.pairingRepositoryProvider = pairingRepositoryProvider;
   }
 
-  public static MembersInjector<MainActivity> create(Provider<FirebaseAuth> authProvider,
-      Provider<SharedPreferences> prefsProvider) {
-    return new MainActivity_MembersInjector(authProvider, prefsProvider);
+  public static MembersInjector<MainActivity> create(
+      Provider<PairingRepository> pairingRepositoryProvider) {
+    return new MainActivity_MembersInjector(pairingRepositoryProvider);
   }
 
   @Override
   public void injectMembers(MainActivity instance) {
-    injectAuth(instance, authProvider.get());
-    injectPrefs(instance, prefsProvider.get());
+    injectPairingRepository(instance, pairingRepositoryProvider.get());
   }
 
-  @InjectedFieldSignature("com.pinbridge.otpmirror.MainActivity.auth")
-  public static void injectAuth(MainActivity instance, FirebaseAuth auth) {
-    instance.auth = auth;
-  }
-
-  @InjectedFieldSignature("com.pinbridge.otpmirror.MainActivity.prefs")
-  public static void injectPrefs(MainActivity instance, SharedPreferences prefs) {
-    instance.prefs = prefs;
+  @InjectedFieldSignature("com.pinbridge.otpmirror.MainActivity.pairingRepository")
+  public static void injectPairingRepository(MainActivity instance,
+      PairingRepository pairingRepository) {
+    instance.pairingRepository = pairingRepository;
   }
 }
